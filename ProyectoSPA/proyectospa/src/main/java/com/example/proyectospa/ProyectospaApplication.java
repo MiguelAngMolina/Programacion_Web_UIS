ae775d7bffdf7f7991142f47954deb22b65d164b
SantiChain <117780948+SantiChain@users.noreply.github.com>
2023-02-19 12:56:53 -0500

    Add files via upload

---
+++ ProyectoSPA/proyectospa/src/main/java/com/example/proyectospa/ProyectospaApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectospa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectospaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectospaApplication.class, args);
+	}
+
+}
---
+++ ProyectoSPA/proyectospa/src/test/java/com/example/proyectospa/ProyectospaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectospa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectospaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
