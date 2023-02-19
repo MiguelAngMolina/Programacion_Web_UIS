9f1b7f5299e504cf6b06da62f9289d616734e034
SantiChain <117780948+SantiChain@users.noreply.github.com>
2023-02-19 12:42:08 -0500

    Add files via upload

---
+++ ProyectoSPA/web/src/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import './app.component.css';
+
+import { ViewEncapsulation} from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular';
+
+  constructor(private router:Router){}
+
+  ListarPersona(){
+    this.router.navigate(["listarPersona"]);
+  }
+  NuevaPersona(){
+    this.router.navigate(["addPersona"]);
+  }
+
+  ListarCiudad(){
+    this.router.navigate(["listarCiudad"]);
+  }
+  NuevaCiudad(){
+    this.router.navigate(["addCiudad"])
+  }
+
+  ListarTipoDocumento(){
+    this.router.navigate(["listarTipoDocumento"])
+  }
+
+  NuevoTipoDocumento(){
+    this.router.navigate(["addTipoDocumento"])
+  }
+}
